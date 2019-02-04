22cbb47eebb9cf937403c43196b249c66f0ad68b
August Emmery-Funch <augustemmeryfunch@gmail.com>
2019-02-04 11:53:14 +0100

    yes

--- index/projektOpgave/sketch.js
+++ index/projektOpgave/sketch.js
@@ -1,1 +1,16 @@
-document.cookie = " 	";
+let video;
+
+function setup() {
+  createCanvas(320, 240);
+  video = createCapture(VIDEO);
+  video.size(320, 240);
+  video.hide();
+}
+
+function modelReady() {
+  console.log("Model Ready");
+}
+
+function draw() {
+  image(video, 0, 0, width, width * video.height / video.width);
+}
