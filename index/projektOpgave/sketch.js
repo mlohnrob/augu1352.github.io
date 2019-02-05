d9a89c18a679566b37a1166153b02e8024bbecb9
August Johan Lassen Emmery-Funch <augu1352@brkuv.dk>
2019-02-05 11:04:56 +0100

    add facial recognition

--- index/projektOpgave/sketch.js
+++ index/projektOpgave/sketch.js
@@ -1,23 +1,50 @@
 let video;
 let features;
+let knn;
 
 function setup() {
-  createCanvas(320, 240);
-  video = createCapture(VIDEO);
-  video.size(320, 240);
-  video.hide();
-  features = ml5.featureExtractor('MobileNet', modelReady);
+	createCanvas(320, 240);
+	video = createCapture(VIDEO);
+	video.size(320, 240);
+	video.hide();
+	features = ml5.featureExtractor('MobileNet', modelReady);
+	knn = ml5.KNNClassifier();
 }
+
+function gotResult(error, result) {
+	if (error) {
+		console.error(error);
+	} else {
+		console.log(result);
+	}
+}
+
 
 function modelReady() {
-  console.log("Model Ready");
+	console.log("Model Ready");
 }
 
-function mousePressed() {
-  const logits = features.infer(video);
-  console.log(logits);
+function getResult() {
+	if (knn.getNumLabels() > 0) {
+		const logits = features.infer(video);
+		knn.classify(logits, gotResult);
+	}
+}
+
+function keyPressed() {
+	const logits = features.infer(video);
+	if (key == "1") {
+		knn.addExample(logits, "one");
+		console.log("Nummer 1");
+	} else if (key == "2") {
+		knn.addExample(logits, "two");
+		console.log("Nummer 2");
+	}
+	if (key == " ") {
+
+	}
 }
 
 function draw() {
-  image(video, 0, 0);
-  }
+	image(video, 0, 0);
+}
