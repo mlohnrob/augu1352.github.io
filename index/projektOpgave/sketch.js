0752eb238054cbdda30664673e9faaba30f2e73f
August Emmery-Funch <augustemmeryfunch@gmail.com>
2019-02-05 08:59:53 +0100

    ihsdf

--- index/projektOpgave/sketch.js
+++ index/projektOpgave/sketch.js
@@ -1,10 +1,12 @@
 let video;
+let features;
 
 function setup() {
   createCanvas(320, 240);
   video = createCapture(VIDEO);
   video.size(320, 240);
-  // capture.hide();
+  video.hide();
+  features = ml5.featureExtractor('MobileNet', modelReady);
 }
 
 function modelReady() {
