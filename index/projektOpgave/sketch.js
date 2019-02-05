6e71a22431477f9f778e55705ad369f0c1a7a6cf
August Johan Lassen Emmery-Funch <augu1352@brkuv.dk>
2019-02-05 13:50:17 +0100

    khsz

--- index/projektOpgave/sketch.js
+++ index/projektOpgave/sketch.js
@@ -1,11 +1,12 @@
 let video;
 let features;
 let knn;
+let output;
 
 function setup() {
-	createCanvas(320, 240);
+	createCanvas(480, 480);
 	video = createCapture(VIDEO);
-	video.size(320, 240);
+	video.size(480, 480);
 	video.hide();
 	features = ml5.featureExtractor('MobileNet', modelReady);
 	knn = ml5.KNNClassifier();
@@ -15,7 +16,10 @@ function gotResult(error, result) {
 	if (error) {
 		console.error(error);
 	} else {
+		const confidences = result.confidencesByLabel;
 		console.log(result);
+		output = document.getElementById("output");
+		output.innerHTML = "The Result Is: " + result.label + " with a confidence of " + (confidences[result.label] * 100) + "%";
 	}
 }
 
@@ -45,6 +49,26 @@ function trainSecond() {
 		knn.addExample(logits, "two");
 		console.log("Nummer 2");
 	}
+}
+
+function trainThird() {
+	const logits = features.infer(video);
+	for (i = 0; i < 150; i++) {
+		knn.addExample(logits, "three");
+		console.log("Nummer 3");
+	}
+}
+
+function trainFourth() {
+	const logits = features.infer(video);
+	for (i = 0; i < 150; i++) {
+		knn.addExample(logits, "four");
+		console.log("Nummer 4");
+	}
+}
+
+function changeLabels(text) {
+	console.error(text)
 }
 
 function draw() {
