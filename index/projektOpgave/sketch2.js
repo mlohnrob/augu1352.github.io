a5cacdae37224af57ebaa027d01b4b7b17729871
August Emmery-Funch <augustemmeryfunch@gmail.com>
2019-02-06 20:11:43 +0100

    Update sketch2.js

--- index/projektOpgave/sketch2.js
+++ index/projektOpgave/sketch2.js
@@ -19,10 +19,14 @@ $(document).ready(function() {
 async function run() {
 	// load the models
 	// console.log(faceapi);
-	await faceapi.loadSsdMobilenetv1Model('./models');
-	await faceapi.loadFaceLandmarkModel('./models');
-	await faceapi.loadMtcnnModel('./models');
-	await faceapi.loadFaceRecognitionModel('./models');
+	faceapi.loadSsdMobilenetv1Model('./models');
+	faceapi.loadFaceLandmarkModel('./models');
+	faceapi.loadMtcnnModel("./models");
+	faceapi.loadFaceRecognitionModel('./models');
+	// console.log(mobileNetModelVar);
+	// console.log(faceLandmarkModelVar);
+	// console.log(mtcnnModelVar);
+	// console.log(faceRecognitionModelVar);
 
 	// try to access users webcam and stream the images
 	// to the video element
@@ -39,11 +43,15 @@ async function run() {
 		minFaceSize: 200
 	};
 	// const options = new faceapi.MtcnnOptions(mtcnnParams);
-	const input = document.getElementById('inputVideo');
-	const fullFaceDescriptions = await faceapi.detectAllFaces(input).withFaceLandmarks().withFaceDescriptors();
-	const mtcnnResults = await faceapi.mtcnn(document.getElementById('inputVideo'), mtcnnForwardParams);
+	// const input = document.getElementById('inputVideo');
+	// let fullFaceDescriptions = faceapi.detectAllFaces(videoEl).withFaceLandmarks().withFaceDescriptors();
+	// fullFaceDescriptions = Array.from(fullFaceDescriptions);
+	// console.log(fullFaceDescriptions);
+	let mtcnnResults = faceapi.mtcnn(videoEl, mtcnnForwardParams);
+	mtcnnResults = Array.from(mtcnnResults);
 
-	const detectionsArray = fullFaceDescriptions.map(fd => fd.detection);
+	// const detectionsArray = fullFaceDescriptions.map(fd => fd.detection);
+	// console.log(detectionsArray);
 	// faceapi.drawDetection('overlay', detectionsArray, {
 	// 	withScore: true
 	// });
@@ -62,6 +70,7 @@ async function run() {
 
 const input = document.getElementById('inputVideo');
 input.onplay = async function player() {
+	// console.log("playing...");
 	run();
 	setTimeout(() => player());
 }
