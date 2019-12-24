6584f8f9509c4665b81ed42e18d11a10d0860230
August Emmery-Funch <32144571+augu1352@users.noreply.github.com>
2019-12-25 00:54:25 +0100

    add images

--- far/main.js
+++ far/main.js
@@ -1,4 +1,4 @@
-for (let i = 0; i < 58; i++) {
+for (let i = 0; i < 76; i++) {
     $('.slideshow-container').append(`<div class="mySlides fade"><img src="./assets/image${i}.jpg" /></div>`);
     console.log(`done: ${i}`);
   }
