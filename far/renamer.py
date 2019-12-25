9b71fec25c59444d219234ce7b57a4ceec638462
August Emmery-Funch <32144571+augu1352@users.noreply.github.com>
2019-12-25 01:02:27 +0100

    more random

--- far/renamer.py
+++ far/renamer.py
@@ -2,5 +2,5 @@ import os
 
 for i, file in enumerate(os.listdir("./assets")):
     #print(i, file)
-    os.rename(f"./assets/{file}", f"./assets/image{i}.jpg")
+    os.rename(f"./assets/{file}", f"./assets/image{76-i}.jpg")
     
